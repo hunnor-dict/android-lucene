555f785f2d5b4fe1f75de79267b27123ac95df50
Ádám Z. Kövér <ardavan.hu@gmail.com>
2013-09-11 22:11:45 +0200

    Renamed strings

--- src/net/hunnor/dict/DatabaseActivity.java
+++ src/net/hunnor/dict/DatabaseActivity.java
@@ -48,37 +48,37 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 		// Check external storage state
 		StringBuilder stringBuilder = new StringBuilder();
 		FileManager fileManager = new FileManager();
-		stringBuilder.append(getResources().getString(R.string.database_storage_check)).append("... ");
+		stringBuilder.append(getResources().getString(R.string.database_check_external_storage)).append("... ");
 		if (fileManager.storageWriteable()) {
-			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_write)).append(")");
+			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_write)).append(")");
 		} else if (fileManager.storageReadable()) {
-			stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_only)).append(")");
+			stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_only)).append(")");
 		} else {
-			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_not_ready)).append(")");
+			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_not_ready)).append(")");
 			check = false;
 		}
 
 		// Check application directory
 		if (check) {
 			stringBuilder.append("<br>");
-			stringBuilder.append(getResources().getString(R.string.database_directory_check)).append("... ");
+			stringBuilder.append(getResources().getString(R.string.database_check_app_directory)).append("... ");
 			File appDirectory = new File(fileManager.getAppDirectory());
 			if (appDirectory.exists()) {
 				if (appDirectory.canRead()) {
 					if (appDirectory.canWrite()) {
-						stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_write)).append(")");
+						stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_write)).append(")");
 					} else {
-						stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_only)).append(")");
+						stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_only)).append(")");
 					}
 				} else {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_not_ready)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_not_ready)).append(")");
 					check = false;
 				}
 			} else {
 				if (appDirectory.mkdirs()) {
-					stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_directory_created)).append(")");
+					stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_create_ok)).append(")");
 				} else {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_directory_create_failed)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_create_error)).append(")");
 					check = false;
 				}
 			}
@@ -88,24 +88,24 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 		File indexDirectory = null;
 		if (check) {
 			stringBuilder.append("<br>");
-			stringBuilder.append(getResources().getString(R.string.database_index_directory_check)).append("... ");
+			stringBuilder.append(getResources().getString(R.string.database_check_index_directory)).append("... ");
 			indexDirectory = new File(fileManager.getAppDirectory() + File.separator + LuceneConstants.INDEX_DIR);
 			if (indexDirectory.exists()) {
 				if (indexDirectory.canRead()) {
 					if (indexDirectory.canWrite()) {
-						stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_write)).append(")");
+						stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_write)).append(")");
 					} else {
-						stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_read_only)).append(")");
+						stringBuilder.append("<font color=\"yellow\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_read_only)).append(")");
 					}
 				} else {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_not_ready)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_not_ready)).append(")");
 					check = false;
 				}
 			} else {
 				if (indexDirectory.mkdirs()) {
-					stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_directory_created)).append(")");
+					stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.io_create_ok)).append(")");
 				} else {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_directory_create_failed)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_create_error)).append(")");
 					check = false;
 				}
 			}
@@ -117,7 +117,7 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 			if (files.length == 0) {
 				stringBuilder.append(getResources().getString(R.string.database_index_directory_empty));
 			} else {
-				stringBuilder.append(getResources().getString(R.string.database_index_check)).append("... ");
+				stringBuilder.append(getResources().getString(R.string.database_check_index_integrity)).append("... ");
 				try {
 					StringBuilder listBuilder = new StringBuilder();
 					Directory directory;
@@ -126,10 +126,10 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 					long lastMod = IndexReader.lastModified(directory);
 					Date date = new Date(lastMod);
 					SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
-					listBuilder.append("<br>").append(getResources().getString(R.string.database_last_modified)).append(": ").append(simpleDateFormat.format(date));
+					listBuilder.append("<br>").append(getResources().getString(R.string.last_modified)).append(": ").append(simpleDateFormat.format(date));
 					stringBuilder.append(listBuilder);
 				} catch (IOException e) {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_index_corrupt)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.index_corrupt)).append(")");
 				}
 			}
 		}
@@ -143,30 +143,30 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 		boolean check = true;
 
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(getResources().getString(R.string.database_connectivity_check)).append("... ");
+		stringBuilder.append(getResources().getString(R.string.database_check_internet_connection)).append("... ");
 		FileManager fileManager = new FileManager();
 		if (fileManager.deviceOnline(this)) {
-			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_connectivity_online)).append(")");
+			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.net_online)).append(")");
 		} else {
-			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_connectivity_offline)).append(")");
+			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.net_offline)).append(")");
 			check = false;
 		}
 
 		if (check) {
 			try {
 				stringBuilder.append("<br>");
-				stringBuilder.append(getResources().getString(R.string.database_update_check)).append("... ");
+				stringBuilder.append(getResources().getString(R.string.database_check_update_url)).append("... ");
 				URL url = new URL(LuceneConstants.INDEX_URL);
 				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 				connection.setRequestMethod("HEAD");
-				stringBuilder.append("<br>").append(getResources().getString(R.string.database_last_modified)).append(": ").append(connection.getHeaderField("Last-Modified"));
-				stringBuilder.append("<br>").append(getResources().getString(R.string.database_size)).append(": ").append(connection.getHeaderField("Content-Length"));
+				stringBuilder.append("<br>").append(getResources().getString(R.string.last_modified)).append(": ").append(connection.getHeaderField("Last-Modified"));
+				stringBuilder.append("<br>").append(getResources().getString(R.string.size)).append(": ").append(connection.getHeaderField("Content-Length"));
 			} catch (MalformedURLException exception) {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_update_malformed_url)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.net_malformed_url)).append(")");
 			} catch (ProtocolException exception) {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_update_protocol_exception)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.net_protocol_exception)).append(")");
 			} catch (IOException exception) {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_update_io_exception)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_exception)).append(")");
 			}
 		}
 		
@@ -179,49 +179,49 @@ public class DatabaseActivity extends Activity implements View.OnClickListener {
 		boolean check = true;
 
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(getResources().getString(R.string.database_connectivity_check)).append("... ");
+		stringBuilder.append(getResources().getString(R.string.database_check_internet_connection)).append("... ");
 		FileManager fileManager = new FileManager();
 		if (fileManager.deviceOnline(this)) {
-			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.database_connectivity_online)).append(")");
+			stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font> (").append(getResources().getString(R.string.net_online)).append(")");
 		} else {
-			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_connectivity_offline)).append(")");
+			stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.net_offline)).append(")");
 			check = false;
 		}
 
 		if (check) {
 			stringBuilder.append("<br>");
-			stringBuilder.append(getResources().getString(R.string.database_download_begin)).append("... ");
+			stringBuilder.append(getResources().getString(R.string.database_download)).append("... ");
 			if (fileManager.downloadFile(LuceneConstants.INDEX_URL, fileManager.getAppDirectory() + File.separator + LuceneConstants.INDEX_ZIP)) {
 				stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font>");
 			} else {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_download_fail)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.net_download_error)).append(")");
 				check = false;
 			}
 		}
 
 		if (check) {
 			stringBuilder.append("<br>");
-			stringBuilder.append(getResources().getString(R.string.database_extract_begin)).append("... ");
+			stringBuilder.append(getResources().getString(R.string.database_extract)).append("... ");
 			if (fileManager.unZip(fileManager.getAppDirectory() + File.separator + LuceneConstants.INDEX_ZIP, fileManager.getAppDirectory())) {
 				stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font>");
 			} else {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_extract_fail)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_extract_error)).append(")");
 				check = false;
 			}
 		}
 
 		if (check) {
 			stringBuilder.append("<br>");
-			stringBuilder.append(getResources().getString(R.string.database_install_begin)).append("... ");
+			stringBuilder.append(getResources().getString(R.string.database_install)).append("... ");
 			if (fileManager.deleteDirectory(fileManager.getAppDirectory() + File.separator + LuceneConstants.INDEX_DIR)) {
 				File f = new File(fileManager.getAppDirectory() + File.separator + "hunnor-lucene-index");
 				if (f.renameTo(new File(fileManager.getAppDirectory() + File.separator + LuceneConstants.INDEX_DIR))) {
 					stringBuilder.append("<font color=\"green\"><b>").append(getResources().getString(R.string.ok)).append("</b></font>");
 				} else {
-					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_install_rename_fail)).append(")");
+					stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_rename_error)).append(")");
 				}
 			} else {
-				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.database_install_delete_fail)).append(")");
+				stringBuilder.append("<font color=\"red\"><b>").append(getResources().getString(R.string.error)).append("</b></font> (").append(getResources().getString(R.string.io_delete_error)).append(")");
 			}
 		}
 
