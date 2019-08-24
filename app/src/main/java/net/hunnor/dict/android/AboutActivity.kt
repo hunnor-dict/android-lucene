8cc972c9ba8ce634fbd3e7d6ea593e1fdeed679b
Ádám Z. Kövér <adamzkover@gmail.com>
2019-08-24 22:39:03 +0200

    Use Kotlin in AboutActivity

---
+++ app/src/main/java/net/hunnor/dict/android/AboutActivity.kt
@@ -0,0 +1,12 @@
+package net.hunnor.dict.android
+
+import android.os.Bundle
+
+class AboutActivity : ActivityTemplate() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+}
