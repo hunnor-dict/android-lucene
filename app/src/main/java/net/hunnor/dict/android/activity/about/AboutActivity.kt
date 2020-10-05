84893562136b6dcb2afc48be147e4deec9fa0b98
Ádám Z. Kövér <adamzkover@gmail.com>
2020-10-05 18:16:16 +0200

    Link to Facebook page

--- app/src/main/java/net/hunnor/dict/android/activity/about/AboutActivity.kt
+++ app/src/main/java/net/hunnor/dict/android/activity/about/AboutActivity.kt
@@ -1,14 +1,27 @@
 package net.hunnor.dict.android.activity.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import net.hunnor.dict.android.R
 import net.hunnor.dict.android.activity.ActivityTemplate
+
 
 class AboutActivity : ActivityTemplate() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+    }
+
+    fun openLink(view: View) {
+        val url = view.tag as String
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 
 }
